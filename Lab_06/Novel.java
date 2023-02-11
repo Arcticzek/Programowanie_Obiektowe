187e9bf3eb762702843a6e6fd18f2b9f73eb2482
Arcticzek <92634011+Arcticzek@users.noreply.github.com>
2023-02-11 22:57:56 +0100

    Create Novel.java

---
+++ Lab_06/Novel.java
@@ -0,0 +1,11 @@
+public class Novel extends Book {
+    private final String genre;
+
+    public Novel(String title, String author, int numPages, String genre) {
+        super(title, author, numPages);
+        this.genre = genre;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
